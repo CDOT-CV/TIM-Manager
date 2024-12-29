5ccc58ce628d5d039bdf6150de8bc8ce1f446216
dmccoystephenson <dmccoystephenson@gmail.com>
2024-12-29 09:43:17 -0700

    Removed `@Autowired` annotation from CdotGisConnector constructor

--- cv-data-service-library/src/main/java/com/trihydro/library/helpers/CdotGisConnector.java
+++ cv-data-service-library/src/main/java/com/trihydro/library/helpers/CdotGisConnector.java
@@ -2,7 +2,6 @@ package com.trihydro.library.helpers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -20,7 +19,6 @@ public class CdotGisConnector {
 
   private final Logger logger = LoggerFactory.getLogger(CdotGisConnector.class);
 
-  @Autowired
   public CdotGisConnector(RestTemplateProvider _restTemplateProvider) {
     this.restTemplateProvider = _restTemplateProvider;
   }
