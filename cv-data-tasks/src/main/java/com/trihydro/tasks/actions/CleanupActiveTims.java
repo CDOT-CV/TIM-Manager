a28a7f658a0e2cb9f0b7563c7a5667dd023f8f26
Marc Wodahl <56242265+mwodahl@users.noreply.github.com>
2025-01-23 15:36:07 -0700

    Add milepost cache clear call to CleanupActiveTims run method

--- cv-data-tasks/src/main/java/com/trihydro/tasks/actions/CleanupActiveTims.java
+++ cv-data-tasks/src/main/java/com/trihydro/tasks/actions/CleanupActiveTims.java
@@ -17,51 +17,57 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
+import com.trihydro.library.service.MilepostService;
+
 @Component
 public class CleanupActiveTims implements Runnable {
     private DataTasksConfiguration configuration;
     private Utility utility;
     private ActiveTimService activeTimService;
     private RestTemplateProvider restTemplateProvider;
+    private MilepostService milepostService;
 
     @Autowired
     public void InjectDependencies(DataTasksConfiguration _configuration, Utility _utility,
-            ActiveTimService _activeTimService, RestTemplateProvider _restTemplateProvider) {
+            ActiveTimService _activeTimService, RestTemplateProvider _restTemplateProvider, MilepostService _milepostService) {
         configuration = _configuration;
         utility = _utility;
         activeTimService = _activeTimService;
         restTemplateProvider = _restTemplateProvider;
+        milepostService = _milepostService;
     }
 
+    @Override
     public void run() {
         utility.logWithDate("Running...", this.getClass());
 
         try {
-            List<ActiveTim> activeTims = new ArrayList<ActiveTim>();
-            List<ActiveTim> tmp = null;
+            List<ActiveTim> activeTims = new ArrayList<>();
+            List<ActiveTim> tmp;
 
             // select active tims missing ITIS codes
             tmp = activeTimService.getActiveTimsMissingItisCodes();
-            if (tmp.size() > 0) {
+            if (tmp.isEmpty()) {
+            } else {
                 utility.logWithDate("Found " + tmp.size() + " Active TIMs missing ITIS Codes", this.getClass());
                 activeTims.addAll(tmp);
             }
 
             // add active tims that weren't sent to the SDX or any RSUs
             tmp = activeTimService.getActiveTimsNotSent();
-            if (tmp.size() > 0) {
+            if (!tmp.isEmpty()) {
                 utility.logWithDate("Found " + tmp.size() + " Active TIMs that weren't distributed", this.getClass());
                 activeTims.addAll(tmp);
             }
 
-            if (activeTims.size() == 0) {
+            if (!activeTims.isEmpty()) {
                 utility.logWithDate("Found 0 Active TIMs", this.getClass());
             }
 
             // delete from rsus and the SDX
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
-            HttpEntity<String> entity = null;
+            HttpEntity<String> entity;
             String activeTimJson;
             Gson gson = new Gson();
 
@@ -69,7 +75,7 @@ public class CleanupActiveTims implements Runnable {
             for (ActiveTim activeTim : activeTims) {
 
                 activeTimJson = gson.toJson(activeTim);
-                entity = new HttpEntity<String>(activeTimJson, headers);
+                entity = new HttpEntity<>(activeTimJson, headers);
 
                 utility.logWithDate(
                         "CleanupActiveTims - Deleting ActiveTim: { activeTimId: " + activeTim.getActiveTimId() + " }",
@@ -77,6 +83,10 @@ public class CleanupActiveTims implements Runnable {
                 restTemplateProvider.GetRestTemplate().exchange(configuration.getWrapperUrl() + "/delete-tim/",
                         HttpMethod.DELETE, entity, String.class);
             }
+
+            // clear the milepostService cache to ensure that the milepost data is up to date
+            utility.logWithDate("Deleting milepost cache", this.getClass());
+            milepostService.clearMilepostCache();
         } catch (Exception e) {
             e.printStackTrace();
             // don't rethrow error, or the task won't be reran until the service is
