e857ee6b1c6b08b51da79acc6785fceff1ac8aca
Marc Wodahl <56242265+mwodahl@users.noreply.github.com>
2025-04-16 14:32:58 -0600

    Add milepostService to VSL controller

--- ode-wrapper/src/main/java/com/trihydro/odewrapper/controller/WydotTimVslController.java
+++ ode-wrapper/src/main/java/com/trihydro/odewrapper/controller/WydotTimVslController.java
@@ -14,6 +14,7 @@ import com.trihydro.library.model.ActiveTim;
 import com.trihydro.library.model.ContentEnum;
 import com.trihydro.library.model.WydotTim;
 import com.trihydro.library.service.ActiveTimService;
+import com.trihydro.library.service.MilepostService;
 import com.trihydro.library.service.RestTemplateProvider;
 import com.trihydro.library.service.TimTypeService;
 import com.trihydro.library.service.WydotTimService;
@@ -46,9 +47,9 @@ public class WydotTimVslController extends WydotTimBaseController {
     public WydotTimVslController(BasicConfiguration _basicConfiguration, WydotTimService _wydotTimService,
             TimTypeService _timTypeService, SetItisCodes _setItisCodes, ActiveTimService _activeTimService,
             RestTemplateProvider _restTemplateProvider, MilepostReduction _milepostReduction, Utility _utility,
-            TimGenerationHelper _timGenerationHelper) {
+            TimGenerationHelper _timGenerationHelper, MilepostService _milepostService) {
         super(_basicConfiguration, _wydotTimService, _timTypeService, _setItisCodes, _activeTimService,
-                _restTemplateProvider, _milepostReduction, _utility, _timGenerationHelper);
+                _restTemplateProvider, _milepostReduction, _utility, _timGenerationHelper, _milepostService);
     }
 
     @RequestMapping(value = "/vsl-tim", method = RequestMethod.POST, headers = "Accept=application/json")
