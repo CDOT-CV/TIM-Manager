474e10725f484727447c3494f0495c8413b5c025
Paige Monington <pmonington@trihydro.com>
2025-12-16 11:30:21 -0700

    chore: remove unneeded logic

--- cv-data-service-library/src/main/java/com/trihydro/library/helpers/CdotGisConnector.java
+++ cv-data-service-library/src/main/java/com/trihydro/library/helpers/CdotGisConnector.java
@@ -20,7 +20,7 @@ import java.util.*;
 public class CdotGisConnector {
     private final String baseUrl = "https://dtdapps.codot.gov/server/rest/services/LRS/Routes_withDEC/MapServer/exts/LrsServerRounded";
     private final int tolerance = 10000;
-    private final int SR = 4326;
+    private final int sr = 4326;
 
     private final RestTemplateProvider restTemplateProvider;
 
@@ -52,7 +52,7 @@ public class CdotGisConnector {
     public ResponseEntity<String> getRouteById(String routeId) throws RestClientException {
         String targetUrl = baseUrl + "/Route";
         logger.info("Getting route with ID {} from CDOT GIS service at: {}", routeId, targetUrl);
-        String params = "?routeId=" + routeId + "&outSR=" + SR + "&f=json";
+        String params = "?routeId=" + routeId + "&outSR=" + sr + "&f=json";
         HttpHeaders headers = new HttpHeaders();
         headers.set("Accept", "application/json");
         HttpEntity<String> entity = new HttpEntity<>(headers);
@@ -79,14 +79,12 @@ public class CdotGisConnector {
                 .queryParam("x", longitude.toPlainString())
                 .queryParam("y", latitude.toPlainString())
                 .queryParam("tolerance", tolerance)
-                .queryParam("outSR", SR)
-                .queryParam("inSR", SR)
-                .queryParam("f", "pjson")
+                .queryParam("outSR", sr)
+                .queryParam("inSR", sr)
+                .queryParam("f", "json")
                 .build(true)
                 .toUri();
 
-        // Log entire request URL
-        logger.warn("Requesting URL: {}", targetUrl);
         HttpHeaders headers = new HttpHeaders();
         headers.set("Accept", "application/json");
         HttpEntity<String> entity = new HttpEntity<>(headers);
@@ -106,46 +104,18 @@ public class CdotGisConnector {
      * @throws RestClientException if an error occurs while making the request
      */
     public ResponseEntity<String> getRouteBetweenMeasures(String routeId, double startMeasure, double endMeasure) throws RestClientException {
-
-        if (isRouteDescending(startMeasure, endMeasure)) {
-            routeId = routeId.replace("_DEC", "") + "_DEC";
-        }
-
-        if (isRouteIDDescending(routeId)) {
-            if (startMeasure < endMeasure) {
-                double newEndMeasure = startMeasure;
-                startMeasure = endMeasure;
-                endMeasure = newEndMeasure;
-            }
-        } else {
-            if (startMeasure > endMeasure) {
-                double newEndMeasure = startMeasure;
-                startMeasure = endMeasure;
-                endMeasure = newEndMeasure;
-            }
-        }
-
+        logger.info("Getting route between measure {} to {} on route {}", startMeasure, endMeasure, routeId);
         String targetUrl = baseUrl + "/RouteBetweenMeasures?";
-        logger.warn("hitting: " + targetUrl);
-        logger.info("Getting route route between measures {} and {} from CDOT GIS service at: {}", startMeasure, endMeasure, targetUrl);
         List<String> parameters = new ArrayList<>();
         parameters.add("routeId=" + routeId);
         parameters.add("fromMeasure=" + startMeasure);
         parameters.add("toMeasure=" + endMeasure);
-        parameters.add("outSR=" + SR);
-        parameters.add("f=pjson");
+        parameters.add("outSR=" + sr);
+        parameters.add("f=json");
         String paramString = String.join("&", parameters);
         HttpHeaders headers = new HttpHeaders();
         headers.set("Accept", "application/json");
         HttpEntity<String> entity = new HttpEntity<>(headers);
         return restTemplateProvider.GetRestTemplate().exchange(targetUrl + paramString, HttpMethod.GET, entity, String.class);
-    }
-
-    private boolean isRouteDescending(double startMeasure, double endMeasure) {
-        return endMeasure < startMeasure;
-    }
-
-    private boolean isRouteIDDescending(String routeId) {
-        return routeId.toLowerCase().endsWith("_dec");
     }
 }
