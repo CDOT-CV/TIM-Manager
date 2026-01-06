f502606325eaf86d1769a5268740ead8430467e2
Paige Monington <pmonington@trihydro.com>
2026-01-06 13:49:29 -0700

    chore: replace explicit getters and setters with Lombok annotations in DataControllerConfigProperties

--- cv-data-controller/src/main/java/com/trihydro/cvdatacontroller/model/DataControllerConfigProperties.java
+++ cv-data-controller/src/main/java/com/trihydro/cvdatacontroller/model/DataControllerConfigProperties.java
@@ -12,6 +12,8 @@ import com.trihydro.library.service.RestTemplateProvider;
 import com.trihydro.library.tables.LoggingTables;
 import com.trihydro.library.tables.TimDbTables;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
@@ -19,6 +21,8 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties("config")
 @Data
+@Getter
+@Setter
 @Import({ TimDbTables.class, SQLNullHandler.class, Utility.class, EmailHelper.class,
     JavaMailSenderImplProvider.class, LoggingTables.class, DbInteractions.class,
     GISConnector.class, RestTemplateProvider.class })
@@ -38,91 +42,4 @@ public class DataControllerConfigProperties implements DbInteractionsProps, Emai
 
     private String milepostProvider;
 
-    public String getDbUrl() {
-        return dbUrl;
-    }
-
-    public void setDbUrl(String dbUrl) {
-        this.dbUrl = dbUrl;
-    }
-
-    public String getDbUsername() {
-        return dbUsername;
-    }
-
-    public void setDbUsername(String dbUsername) {
-        this.dbUsername = dbUsername;
-    }
-
-    public String getDbPassword() {
-        return dbPassword;
-    }
-
-    public void setDbPassword(String dbPassword) {
-        this.dbPassword = dbPassword;
-    }
-
-    public int getMaximumPoolSize() {
-        return maximumPoolSize;
-    }
-
-    public void setMaximumPoolSize(int maximumPoolSize) {
-        this.maximumPoolSize = maximumPoolSize;
-    }
-
-    public int getConnectionTimeout() {
-        return connectionTimeout;
-    }
-
-    public void setConnectionTimeout(int connectionTimeout) {
-        this.connectionTimeout = connectionTimeout;
-    }
-
-    public String[] getAlertAddresses() {
-        return alertAddresses;
-    }
-
-    public void setAlertAddresses(String[] alertAddresses) {
-        this.alertAddresses = alertAddresses;
-    }
-
-    public String getFromEmail() {
-        return fromEmail;
-    }
-
-    public void setFromEmail(String fromEmail) {
-        this.fromEmail = fromEmail;
-    }
-
-    public String getEnvironmentName() {
-        return environmentName;
-    }
-
-    public void setEnvironmentName(String environmentName) {
-        this.environmentName = environmentName;
-    }
-
-    public String getMailHost() {
-        return mailHost;
-    }
-
-    public void setMailHost(String mailHost) {
-        this.mailHost = mailHost;
-    }
-
-    public int getMailPort() {
-        return mailPort;
-    }
-
-    public void setMailPort(int mailPort) {
-        this.mailPort = mailPort;
-    }
-
-    public String getMilepostProvider() {
-        return milepostProvider;
-    }
-
-    public void setMilepostProvider(String milepostProvider) {
-        this.milepostProvider = milepostProvider;
-    }
 }
